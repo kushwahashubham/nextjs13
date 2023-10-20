8fded08c9e6415b01101c21b9037716cba8033e4
Shubham Kushwaha <shubham.kushwaha.star@gmail.com>
2023-10-20 16:55:58 +0530

    add auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Next.js 13 and we&apos;re comming!</h1>
-      <h2 className="h2-bold">Next.js 13 and we&apos;re comming!</h2>
-      <h3 className="h3-bold">Next.js 13 and we&apos;re comming!</h3>
-    </div>
-  );
-}
