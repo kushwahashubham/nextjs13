24135e0d2cfd9f965b699ee31f0e3396fc6eaa3d
Shubham Kushwaha <shubham.kushwaha.star@gmail.com>
2023-12-09 23:35:53 +0530

    updated public routes

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     "/",
-    "/api/webhook(.*)",
+    "/api/webhooks(.*)",
     "question/:id",
     "tags",
     "tags/:id",
